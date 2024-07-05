e2779666fb5fac3b36c4424b7eac327afc16893b
Rahima Akter <139994153+CODERRAHIMA@users.noreply.github.com>
2024-07-05 23:48:06 +0600

    Add files via upload
    adding week_14

---
+++ week_14/day_2/A_Mathematical_Problem.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pb push_back
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+using namespace std;
+const int INF = 0x3f3f3f3f;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int TC;
+    cin >> TC;
+    while (TC--)
+    {
+        int n, ans = INF;
+        string s;
+        cin >> n >> s;
+        for (int i = 1; i < n; i++)
+        {
+            int num = stoi(s.substr(i - 1, 2)); // 找两位数
+            for (int j = 0; j < n; j++)
+            {
+                if (j == i - 1 || j == i)
+                    continue;
+                int temp = s[j] - '0';
+                num = min(num * temp, num + temp); // 选定两位数后 选择+ x符号
+            }
+            ans = min(ans, num);
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
